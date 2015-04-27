7ff6483214ec99438450963eb01990bda18a483f
Yoann QUERET <yoann@queret.net>
2015-04-27 23:51:06 +0200

    API refactor and add default value for optional argv

--- api.py
+++ api.py
@@ -6,39 +6,37 @@ from datetime import datetime
 from datetime import timedelta
 import json
 import os
+import sys
 
 import argparse
 
 
 # ---------- API ------------------------------------------------------------
 
-class api(object):
+class API:
+	exposed = True
+	def __init__(self):
+		self.getConfig = getConfig()
+		#self.setMetadata = setMetadata()
+
+	def GET(self):
+		return "api - Your IP is %s " % ( cherrypy.request.remote.ip )
+
+
+class getConfig:
+	exposed = True
 	def __init__(self):
 		pass
-		
-	def index(self):
-		output = '<h1>ODR - Encoder Manager API</h1>'
-		output += '<ul>'
-		output += '<li><a href="/api/metadata">metadata</a> : Put metadata</li>'
-		output += '</ul>'
-		return output
-	index.exposed = True
-	
-	def config(self):
-		output = 'config'
-		return output
-	config.exposed = True
-	
-	def metadata(self):
-		output = 'metadata'
-		return output
-	metadata.exposed = True
 
+	def GET(self):
+		output = []
+		return json.dumps(output)
 
 # ---------- MAIN ------------------------------------------------------------
 class root(object):
 	def __init__(self):
-		self.api = api()
+		#self.api = API()
+		pass
 		
 	def index(self):
 		raise cherrypy.HTTPRedirect('/home')
@@ -54,53 +52,76 @@ class root(object):
 if __name__ == '__main__':
 	# Get configuration file in argument
 	parser = argparse.ArgumentParser(description='ODR WebGUI Encoder Manager')
-	parser.add_argument('-s','--static-dir', help='Fulle path of static directory content',required=True)
-	parser.add_argument('-l','--log-dir', help='log directory full path',required=True)
-	parser.add_argument('--host', help='socket host (default: 0.0.0.0)',required=False)
-	parser.add_argument('--port', help='socket port (default: 8080)',required=False)
-	parser.add_argument('--rpc-host', help='encoder RPC host (default: 127.0.0.1)',required=False)
-	parser.add_argument('--rpc-port', help='encoder RPC port (default: 7780)',required=False)
+	parser.add_argument('-s','--static_dir', help='Full path of static directory content',required=True)
+	parser.add_argument('-l','--log_dir', help='log directory full path',required=True)
+	parser.add_argument('--host', default='0.0.0.0', help='socket host (default: 0.0.0.0)',required=False)
+	parser.add_argument('--port', default='8080', help='socket port (default: 8080)',required=False)
+	parser.add_argument('--rpc-host', default='127.0.0.1', help='encoder RPC host (default: 127.0.0.1)',required=False)
+	parser.add_argument('--rpc-port', default='7780', help='encoder RPC port (default: 7780)',required=False)
 	cli_args = parser.parse_args()
 	
+	# Check if log_dir exist and is writeable
+	if os.path.isdir(cli_args.log_dir) and os.access(cli_args.log_dir, os.W_OK):
+		print "Use logging directory %s" % (cli_args.log_dir)
+	else:
+		print "Log directory not exist or is not writeable - %s" % (cli_args.log_dir)
+		sys.exit(1)
 	
+	# Check if static_dir exist and is readeable
+	if os.path.isdir(cli_args.static_dir) and os.access(cli_args.static_dir, os.R_OK):
+		print "Use static directory %s" % (cli_args.static_dir)
+	else:
+		print "Static directory not exist or is not readeable - %s" % (cli_args.static_dir)
+		sys.exit(1)
+	
+	# Start cherrypy
 	cherrypy.process.plugins.Daemonizer(cherrypy.engine).subscribe()
-	cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': 8080, 'request.show_tracebacks' : True, })
+	cherrypy.config.update({'server.socket_host': cli_args.host, 'server.socket_port': int(cli_args.port), 'request.show_tracebacks' : True, })
+	cherrypy.tree.mount(
+		API(), '/api/',
+			{'/':
+				{'log.access_file' : os.path.join(cli_args.log_dir, 'access.log'),
+				'log.screen': False,
+				'tools.sessions.on': True,
+				'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
+			}
+		)
 	cherrypy.quickstart(root(),config={
   '/':
-		{ 'log.access_file' : './access.log',
+		{ 'log.access_file' : os.path.join(cli_args.log_dir, 'access.log'),
 		'log.screen': False,
 		'tools.sessions.on': True
 		},
   '/home':
-		{ 'log.access_file' : './access.log',
+		{ 'log.access_file' : os.path.join(cli_args.log_dir, 'access.log'),
 		'log.screen': False,
 		'tools.sessions.on': True,
 		'tools.staticfile.on': True,
 		'tools.staticfile.filename': os.path.join(os.path.abspath("."), u"static/home.html")
 		},
   '/config':
-		{ 'log.access_file' : './access.log',
+		{ 'log.access_file' : os.path.join(cli_args.log_dir, 'access.log'),
 		'log.screen': False,
 		'tools.sessions.on': True,
 		'tools.staticfile.on': True,
 		'tools.staticfile.filename': os.path.join(os.path.abspath("."), u"static/config.html")
 		},
   '/css':
-		{ 'log.access_file' : './access.log',
+		{ 'log.access_file' : os.path.join(cli_args.log_dir, 'access.log'),
 		'log.screen': False,
 		'tools.sessions.on': True,
 		'tools.staticdir.on': True,
 		'tools.staticdir.dir': os.path.join(os.path.abspath("."), u"static/css/")
 		},
   '/js':
-		{ 'log.access_file' : './access.log',
+		{ 'log.access_file' : os.path.join(cli_args.log_dir, 'access.log'),
 		'log.screen': False,
 		'tools.sessions.on': True,
 		'tools.staticdir.on': True,
 		'tools.staticdir.dir': os.path.join(os.path.abspath("."), u"static/js/")
 		},
   '/fonts':
-		{ 'log.access_file' : './access.log',
+		{ 'log.access_file' : os.path.join(cli_args.log_dir, 'access.log'),
 		'log.screen': False,
 		'tools.sessions.on': True,
 		'tools.staticdir.on': True,

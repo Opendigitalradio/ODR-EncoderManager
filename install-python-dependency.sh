e9446d4762f87416e03b1876cebe05c72a5e1769
Yoann QUERET <yoann@queret.net>
2015-05-15 14:59:33 +0200

    Add cherrypy python modules as needed

--- install-python-dependency.sh
+++ install-python-dependency.sh
@@ -6,8 +6,12 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+# install pip
 wget https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py
 python /tmp/get-pip.py
+
+# for both encoder and api daemon
+pip install argparse
 
 # for encoder daemon
 pip install twisted
@@ -15,3 +19,4 @@ pip install txJSON-RPC
 
 # for api daemon
 pip install requests
+pip install cherrypy

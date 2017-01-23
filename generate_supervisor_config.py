b5c2563fd87542e70ac8f0a8a61b147296b7e859
Yoann QUERET <yoann@queret.net>
2017-01-23 18:18:49 +0100

    Add cli tools to generate supervisor config file from config.json

---
+++ generate_supervisor_config.py
@@ -0,0 +1,32 @@
+# -*- encoding: UTF-8 -*-
+#
+# Form based authentication for CherryPy. Requires the
+# Session tool to be loaded.
+#
+
+import os
+import argparse
+from config import Config
+
+if __name__ == '__main__':
+
+	# Get configuration file in argument
+	parser = argparse.ArgumentParser(description='ODR Encoder Manager (Tools to generate supervisor config file)')
+	parser.add_argument('-c','--config', help='configuration filename',required=True)
+	cli_args = parser.parse_args()
+	
+	# Check if configuration exist and is readable
+	if os.path.isfile(cli_args.config) and os.access(cli_args.config, os.R_OK):
+		print "Use configuration file %s" % (cli_args.config)
+	else:
+		print "Configuration file is missing or is not readable - %s" % (cli_args.config)
+		sys.exit(1)
+		
+	# Load configuration
+	config = Config(cli_args.config)
+
+	# Generate supervisor files
+	try:
+		config.generateSupervisorFiles(config.config)
+	except Exception,e:
+		print 'Error generating supervisor files', str(e)

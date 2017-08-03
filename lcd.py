0c45e6e399b8d6600ca2d1edc593d421321cbd2f
Yoann QUERET <yoann@queret.net>
2017-08-03 16:40:05 +0000

    Add interface to manage network interface

--- lcd.py
+++ lcd.py
@@ -8,6 +8,7 @@ import os
 import socket
 import argparse
 
+from api import API
 from config import Config
 from lcd import LcdMatrix
 
@@ -17,107 +18,113 @@ LCD_COLS = 16 # Taille du LCD 16 caractères x 2 lignes
 LCD_ROWS = 2
 
 def wtitle(title, msg):
-	lcd.clear_screen()
-	lcd.position( 1, 1 )
-	lcd.write( title )
-	
-	if len(msg) <= 16:
-		lcd.position( 2, 1 )
-                lcd.write( msg )
-		time.sleep(2)
-	else:
-		i=0
-		while i != len(msg)-15:
-			m = msg[i:i+16]
-			lcd.position( 2, 1 )
-			lcd.write( m )
-			if i == 0:
-				time.sleep(2)
-			else:
-				time.sleep(0.5)
-			i=i+1
-		time.sleep(1)
+    lcd.clear_screen()
+    lcd.position( 1, 1 )
+    lcd.write( title )
+    
+    if len(msg) <= 16:
+        lcd.position( 2, 1 )
+        lcd.write( msg )
+        time.sleep(2)
+    else:
+        i=0
+        while i != len(msg)-15:
+            m = msg[i:i+16]
+            lcd.position( 2, 1 )
+            lcd.write( m )
+            if i == 0:
+                time.sleep(2)
+            else:
+                time.sleep(0.5)
+            i=i+1
+        time.sleep(1)
 
 if __name__ == '__main__':
-	# Get configuration file in argument
-	parser = argparse.ArgumentParser(description='ODR Encoder Manager (LCD)')
-	parser.add_argument('-c','--config', help='configuration filename',required=True)
-	cli_args = parser.parse_args()
-	
-	# Check if configuration exist and is readable
-	if os.path.isfile(cli_args.config) and os.access(cli_args.config, os.R_OK):
-		print "Use configuration file %s" % (cli_args.config)
-	else:
-		print "Configuration file is missing or is not readable - %s" % (cli_args.config)
-		sys.exit(1)
-		
-	# Load configuration
-	config = Config(cli_args.config)
+    # Get configuration file in argument
+    parser = argparse.ArgumentParser(description='ODR Encoder Manager (LCD)')
+    parser.add_argument('-c','--config', help='configuration filename',required=True)
+    cli_args = parser.parse_args()
+    
+    # Check if configuration exist and is readable
+    if os.path.isfile(cli_args.config) and os.access(cli_args.config, os.R_OK):
+        print "Use configuration file %s" % (cli_args.config)
+    else:
+        print "Configuration file is missing or is not readable - %s" % (cli_args.config)
+        sys.exit(1)
+        
+    # Load configuration
+    config = Config(cli_args.config)
 
-	# Load LcdMatrix
-	lcd = LcdMatrix( PORT_SERIE )
-		
-	# Initialiser la taille du LCD (et sauver dans l'EEPROM)
-	lcd.set_lcd_size( LCD_COLS, LCD_ROWS )
-	lcd.clear_screen()
-	
-	# Set splash screen
-	lcd.set_splashscreen( 'DAB+ Encoder    Starting ...    ' )
-	
-	# Activer le rétro-éclairage
-	lcd.activate_lcd( True )
-	
-	# Luminosité max
-	lcd.brightness( 255 )
-	
-	#  Background RGB
-	lcd.color( 120, 120, 255 ) 
-	
-	while True:
-		# Retreive NETWORK IFACE IP
-		try:
-			admin_ip = os.popen('ip addr show eth0').read().split("inet ")[1].split("/")[0]
-		except:
-			admin_ip = False
-			
-		try:
-			lan_ip = os.popen('ip addr show eth1').read().split("inet ")[1].split("/")[0]
-		except:
-			lan_ip = False
-			
-		try:
-			wan_ip = os.popen('ip addr show eth2').read().split("inet ")[1].split("/")[0]
-		except:
-			wan_ip = False
-		
-		lcd.autoscroll( False )
-		# Display hostname and admin ip
-		lcd.clear_screen()
-		lcd.write( 'DAB+ Encoder')
-		time.sleep( 2 )
-		if admin_ip:
-			lcd.clear_screen()
-			lcd.write( 'DAB+ Encoder ADM')
-			lcd.position( 2, 1 )
-			lcd.write( admin_ip )
-			time.sleep( 2 )
-		if lan_ip:
-			lcd.clear_screen()
-			lcd.write( 'DAB+ Encoder LAN')
-			lcd.position( 2, 1 )
-			lcd.write( lan_ip )
-			time.sleep( 2 )
-		if wan_ip:
-			lcd.clear_screen()
-			lcd.write( 'DAB+ Encoder WAN')
-			lcd.position( 2, 1 )
-			lcd.write( wan_ip )
-			time.sleep( 2 )
-		
-		with open(config.config['odr']['padenc']['dls_fifo_file'], 'r') as file:
-			for line in file:
-				dls = line.strip()
-				
-		wtitle('DAB+ Encoder DLS', dls)
-		
+    # Load LcdMatrix
+    lcd = LcdMatrix( PORT_SERIE )
+        
+    # Initialiser la taille du LCD (et sauver dans l'EEPROM)
+    lcd.set_lcd_size( LCD_COLS, LCD_ROWS )
+    lcd.clear_screen()
+    
+    # Set splash screen
+    lcd.set_splashscreen( 'DAB+ Encoder    Starting ...    ' )
+    
+    # Activer le rétro-éclairage
+    lcd.activate_lcd( True )
+    
+    # Luminosité max
+    lcd.brightness( 255 )
+    
+    #  Background RGB
+    lcd.color( 120, 120, 255 ) 
+    
+    while True:
+        # Retreive NETWORK IFACE IP
+        try:
+            config_ip = os.popen('ip addr show eth0').read().split("inet ")[1].split("/")[0]
+        except:
+            config_ip = False
+            
+        try:
+            net1_ip = os.popen('ip addr show eth1').read().split("inet ")[1].split("/")[0]
+        except:
+            net1_ip = False
+        
+        try:
+            net2_ip = os.popen('ip addr show eth2').read().split("inet ")[1].split("/")[0]
+        except:
+            net2_ip = False
+        
+        lcd.autoscroll( False )
+        # Display hostname and admin ip
+        lcd.clear_screen()
+        lcd.write( 'DAB+ Encoder')
+        time.sleep( 2 )
+        if config_ip:
+            lcd.clear_screen()
+            lcd.write( 'IP iface CONFIG')
+            lcd.position( 2, 1 )
+            lcd.write( config_ip )
+            time.sleep( 4 )
+        if net1_ip:
+            lcd.clear_screen()
+            lcd.write( 'IP iface NET1')
+            lcd.position( 2, 1 )
+            lcd.write( net1_ip )
+            time.sleep( 4 )
+        if net2_ip:
+            lcd.clear_screen()
+            lcd.write( 'IP iface NET2')
+            lcd.position( 2, 1 )
+            lcd.write( net2_ip )
+            time.sleep( 4 )
+        
+        with open(config.config['odr']['padenc']['dls_fifo_file'], 'r') as f:
+            for line in f:
+                if line.startswith('#'):
+                    continue
+                if line.startswith('DL_PLUS='):
+                    continue
+                if line.startswith('DL_PLUS_TAG='):
+                    continue
+                dls = line.rstrip()
+                
+        wtitle('DAB+ Encoder DLS', dls)
+        
 
--- static/js/odr-network.js
+++ static/js/odr-network.js
@@ -19,7 +19,103 @@
 // along with ODR-EncoderManager.  If not, see <http://www.gnu.org/licenses/>.
 // 
 
+function requestCards(callback) {
+    $("#network_card").empty();
+    $.ajax({
+        type: "GET",
+        url: "/api/getNetworkCards",
+        contentType: 'application/json',
+        dataType: 'json',
+        
+        error: function(data) {
+            $.gritter.add({
+                title: 'Load network cards : ERROR !',
+                text: data['status'] + " : " + data['statusText'],
+                image: '/fonts/warning.png',
+                sticky: true,
+            });
+        },
+        success: function(data) {
+            if ( data['status'] == '0' ) {
+                var o = new Option('-', '');
+                $("#network_card").append(o);
+                $.each( data['data'], function( section_key, section_val ) {
+                    var o = new Option(section_val['alias'] + ' ('+section_val['card']+')', section_val['card']);
+                    $("#network_card").append(o);
+                });
+            } else {
+                $.gritter.add({
+                    title: 'Load network cards',
+                    text: "ERROR = " + data['status'] + " : " + data['statusText'],
+                    image: '/fonts/warning.png',
+                    sticky: true,
+                });
+            }
+        }
+    });
+}
 
+function setEnableDisableDHCP(){
+    if ($('#network_dhcp').val() == 'true') {
+        $('#network_ip').prop('disabled', true);
+        $('#network_mask').prop('disabled', true);
+        $('#network_gateway').prop('disabled', true);
+    } else {
+        $('#network_ip').prop('disabled', false);
+        $('#network_mask').prop('disabled', false);
+        $('#network_gateway').prop('disabled', false);
+    }
+}
+
+function requestCardInfo(callback) {
+    $.ajax({
+        type: "GET",
+        url: "/api/getNetworkCards?card="+$('#network_card').val(),
+        contentType: 'application/json',
+        dataType: 'json',
+        
+        error: function(data) {
+            $.gritter.add({
+                title: 'Load network cards : ERROR !',
+                text: data['status'] + " : " + data['statusText'],
+                image: '/fonts/warning.png',
+                sticky: true,
+            });
+        },
+        success: function(data) {
+            if ( data['status'] == '0' ) {
+                $('#network_ip').val(data['data']['ip']);
+                $('#network_mask').val(data['data']['netmask']);
+                $('#network_gateway').val(data['data']['gateway']);
+                $('#network_dhcp option[value="'+data['data']['dhcp']+'"]').prop('selected', true);
+                
+                if (data['data']['manage'] == 'true') {
+                    $('#network_dhcp').prop('disabled', false);
+                    $('#network_ip').prop('disabled', false);
+                    $('#network_mask').prop('disabled', false);
+                    $('#network_gateway').prop('disabled', false);
+                    $('#btn_save').prop('disabled', false);
+                    $("#btn_save").removeClass('disabled');
+                    setEnableDisableDHCP();
+                } else {
+                    $('#network_dhcp').prop('disabled', true);
+                    $('#network_ip').prop('disabled', true);
+                    $('#network_mask').prop('disabled', true);
+                    $('#network_gateway').prop('disabled', true);
+                    $("#btn_save").addClass('disabled');
+                }
+                
+            } else {
+                $.gritter.add({
+                    title: 'Load network cards',
+                    text: "ERROR = " + data['status'] + " : " + data['statusText'],
+                    image: '/fonts/warning.png',
+                    sticky: true,
+                });
+            }
+        }
+    });
+}
 
 // ToolTip init
 $(function(){
@@ -28,5 +124,56 @@ $(function(){
 
 // Onload
 $(function(){
+   requestCards();
    
+   $('#network_card').change(function() {
+      requestCardInfo();
+   });
+   
+   $('#network_dhcp').change(function() {
+      setEnableDisableDHCP();
+   });
+   
+   $('#btn_save').click(function() {
+        var param = {
+            "card": $('#network_card').val(),
+            "ip" : $('#network_ip').val(),
+            "netmask" : $('#network_mask').val(),
+            "gateway" : $('#network_gateway').val(),
+            "dhcp" : $('#network_dhcp').val()
+        }
+        $.ajax({
+            type: "POST",
+            url: "/api/setNetworkCard",
+            data: JSON.stringify(param),
+            contentType: 'application/json',
+            dataType: 'text',
+            
+            error: function(data) {
+                $.gritter.add({
+                    title: 'Save',
+                    text: "ERROR = " + data['status'] + " : " + data['statusText'],
+                    image: '/fonts/warning.png',
+                    sticky: true,
+                });
+            },
+            success: function(data) {
+                data = jQuery.parseJSON(data)
+                if ( data['status'] == '0' ) {
+                    $.gritter.add({
+                        title: 'Save',
+                        image: '/fonts/accept.png',
+                        text: 'Ok',
+                    });
+                } else {
+                    $.gritter.add({
+                        title: 'Save',
+                        text: "ERROR = " + data['status'] + " : " + data['statusText'],
+                        image: '/fonts/warning.png',
+                        sticky: true,
+                    });
+                }
+            }
+        });
+    });
 });
